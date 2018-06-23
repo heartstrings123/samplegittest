a38884bffb9cc4d64804c187187923ef9d2a1917
heartstrings123 <durgadevi459@gmail.com>
2018-06-23 11:50:12 +0530

    My Last Commit

--- src/test/java/samplegittest/Samplegittest2.java
+++ src/test/java/samplegittest/Samplegittest2.java
@@ -10,16 +10,16 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
-public class Samplegittest {
+public class Samplegittest2 {
 
 	@Test
 	public void logintest() throws IOException, InterruptedException
 	{
 		WebDriver wd = new FirefoxDriver();
 		wd.manage().window().maximize();
-		wd.get("http://www.bing.com/");
+		wd.get("http://www.google.com/");
 		File src = ((TakesScreenshot)wd).getScreenshotAs(OutputType.FILE);
-		FileUtils.copyFile(src,new File("C:\\Users\\DURGA\\workspace\\samplegittest\\screenshot\\bing.jpeg"));
+		FileUtils.copyFile(src,new File("C:\\Users\\DURGA\\workspace\\samplegittest\\screenshot\\google1.jpeg"));
 		Thread.sleep(30000);
 	
 	}
